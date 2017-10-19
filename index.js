f10856062efe4c9853b3a0217a1a46b218908a98
Jake Robbins <jrobbinsd@gmail.com>
2017-10-19 11:07:16 -0600

    added routes to login page

--- index.js
+++ index.js
@@ -5,19 +5,18 @@ const port = process.env.PORT || 3000
 const bodyParser =require('body-parser')
 const hbs = require('hbs');
 
-
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
 app.set('view engine','hbs')
 
-// app.get('/', (req, res) => {
-// 	res.send('hello world')
-// })
 app.get('/', (req, res) => {
 	res.render('index.hbs')
 })
-
+// route to login page
+app.get('/login.hbs', (req, res) =>{
+	res.render('login.hbs')
+})
 
 app.listen(port, () =>{
 	console.log(`listening at ${port}`);
